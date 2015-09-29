3e47eec57559e4eb416365e115b50c67bf784a1e
Travis Placek <tplacek@uark.edu>
2015-09-29 00:12:09 -0500

    Started test framework

--- manbearpig/zealousduck/tests.py
+++ manbearpig/zealousduck/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import datetime
+
+from django.utils import timezone
+from django.test import TestCase
+
+from .models import Product
+
+class ProductMethodTests(TestCase):
+
+	def test_create_product(self):
+		p = Product();
+		p.description = 'Test Product';
+        	p.itemID = 1337;
+		p.lookupcode = '133713371337'
+		self.assertEqual(p.in_stock(), False)
