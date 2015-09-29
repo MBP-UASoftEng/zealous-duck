0155fe02b5039eaa60f632393bf9a688d6461187
Travis Placek <tplacek@uark.edu>
2015-09-28 20:15:06 -0500

    Added Tables to Django Admin Interface

--- manbearpig/zealousduck/admin.py
+++ manbearpig/zealousduck/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product
+from .models import Employee
+from .models import Transaction
+from .models import TransactionEntry
+from .models import TenderEntry
+
+admin.site.register(Product)
+admin.site.register(Employee)
+admin.site.register(Transaction)
+admin.site.register(TransactionEntry)
+admin.site.register(TenderEntry)
+
