d7fc0a956fe81fb68f2d4651901a8fbf71c1c960
Travis Placek <tplacek@uark.edu>
2015-09-28 21:56:53 -0500

    Friendly product output

--- manbearpig/zealousduck/models.py
+++ manbearpig/zealousduck/models.py
@@ -18,6 +18,9 @@ class Product(models.Model):
         msrp = models.DecimalField(max_digits=10, decimal_places=2)
         dateCreated = models.DateTimeField(auto_now=False, auto_now_add=True)
 
+	def __str__(self):
+		return self.description
+
 class Employee(models.Model):
 	name_first = models.CharField(max_length=100)
 	name_last = models.CharField(max_length=100)
